0c42e0908d681c323f24efb374c449084ec29adc
Oleksii Velychko <katya.shapovalova3012@gmail.com>
2026-02-06 21:00:38 +0200

    Update for deploy

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const teacherDropdown = document.getElementById('teacherDropdown');
 let groupDebounceTimer;
 let teacherDebounceTimer;
 
-const API_BASE = 'http://127.0.0.1:8000/api';
+const API_BASE = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/';
 
 document.addEventListener('DOMContentLoaded', () => {
     loadActiveSemester();
